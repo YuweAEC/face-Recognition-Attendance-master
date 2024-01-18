f17c23d772fa7c3213f8c01619cab7a25abe5f5a
Yuvraj Singh <yuwe18b@gmail.com>
2024-01-18 21:35:04 +0530

    refactor: add import library and update some lines of code

--- Face-Recognition-Attendance-master/yuvi.py
+++ Face-Recognition-Attendance-master/yuvi.py
@@ -3,7 +3,6 @@ import face_recognition
 import os
 import numpy as np
 from datetime import datetime
-import dlib  # Add this import
 
 path = "Face-Recognition-Attendance-master\\Person_Images"
 images = []
@@ -39,4 +38,22 @@ while True:
     faces_in_frame = face_recognition.face_locations(imgS)
     encoded_faces = face_recognition.face_encodings(imgS, faces_in_frame)
     for encode_face, faceloc in zip(encoded_faces, faces_in_frame):
-        matches = face_recognition.compare
+        matches = face_recognition.compare_faces(encoded_face_train, encode_face)
+        face_dist = face_recognition.face_distance(encoded_face_train, encode_face)
+        matchIndex = np.argmin(face_dist)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex]
+            y1, x2, y2, x1 = faceloc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            markAttendance(name)
+
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
